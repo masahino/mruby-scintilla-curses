2831c4a50af2c515fa74e99354444579fefcc653
masahino <masahino@gmail.com>
2022-04-23 09:18:51 +0900

    cosmetic changes

--- test/scintilla_curses.rb
+++ test/scintilla_curses.rb
@@ -2,26 +2,26 @@
 ## Scintilla::ScintillaCurses Test
 ##
 
-assert("new") do
-  sci = Scintilla::ScintillaCurses::new()
+assert('new') do
+  sci = Scintilla::ScintillaCurses.new
   assert_kind_of(Scintilla::ScintillaCurses, sci)
 end
 
-assert("delete") do
-#  sci = Scintilla::ScintillaCurses.new()
-#  sci.delete
+assert('delete') do
+  #  sci = Scintilla::ScintillaCurses.new()
+  #  sci.delete
 end
 
-assert("get_clipboard") do
-  sci = Scintilla::ScintillaCurses.new()
+assert('get_clipboard') do
+  sci = Scintilla::ScintillaCurses.new
   text = sci.get_clipboard
   assert_kind_of(String, text)
 end
 
-assert("set_lexer_language") do
-  sci = Scintilla::ScintillaCurses.new()
-  sci.sci_set_lexer_language("cpp")
-  assert_equal("cpp", sci.sci_get_lexer_language)
-  sci.sci_set_lexer_language("ruby")
-  assert_equal("ruby", sci.sci_get_lexer_language)
+assert('set_lexer_language') do
+  sci = Scintilla::ScintillaCurses.new
+  sci.sci_set_lexer_language('cpp')
+  assert_equal('cpp', sci.sci_get_lexer_language)
+  sci.sci_set_lexer_language('ruby')
+  assert_equal('ruby', sci.sci_get_lexer_language)
 end
